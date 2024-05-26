795acb7de2634f8d91b81c7d975d39b326fcb97c
Sahil Goonraz <sahilgoonrazk@gmail.com>
2024-05-26 15:52:34 +0100

    script now creates webpage display all events of the day

--- display_cal.py
+++ display_cal.py
@@ -1,1 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from quickstart import get_events_info
+import datetime
+import calendar
+
+app = Flask(__name__)
+month = datetime.datetime.now().month
+year = datetime.datetime.now().year
+
+@app.route("/")
+def index():
+    return get_events_info()
+
+app.run(host="0.0.0.0", port=6969)
