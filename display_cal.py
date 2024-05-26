fe2e2276c2e2d8b5fb0084a31db9bfa305b70ea8
Sahil Goonraz <sahilgoonrazk@gmail.com>
2024-05-26 16:20:39 +0100

    ongoing and future events are now detected and stored in the appropriate variables

--- display_cal.py
+++ display_cal.py
@@ -7,8 +7,29 @@ app = Flask(__name__)
 month = datetime.datetime.now().month
 year = datetime.datetime.now().year
 
+def is_event_ongoing(start, end):
+    now = datetime.datetime.now()
+
+    return start.replace(tzinfo=None) <= now <= end.replace(tzinfo=None)
+
+def is_future_event(start):
+    now = datetime.datetime.now()
+
+    return now < start.replace(tzinfo=None)
+
 @app.route("/")
 def index():
-    return get_events_info()
+    events = get_events_info()
+    current_event = None
+    future_events = []
+
+    for event in events:
+        if is_event_ongoing(start=event[0], end=event[1]):
+           current_event = event
+
+        if is_future_event(start=event[0]):
+            future_events.append(event)
+
+    return current_event
 
 app.run(host="0.0.0.0", port=6969)
