e267359fc8434fc696c744fc89b8d381d07a0d16
Sahil Goonraz <sahilgoonrazk@gmail.com>
2024-05-26 18:40:59 +0100

    modified script and template to fix crashing when no events were found currently or in the future

--- display_cal.py
+++ display_cal.py
@@ -23,6 +23,8 @@ def index():
     current_event = None
     future_events = []
 
+    current_name, current_times, upcoming_event = "No events currently", "", "No upcoming events"
+
     for event in events:
         if is_event_ongoing(start=event[0], end=event[1]):
            current_event = event
@@ -30,10 +32,21 @@ def index():
         if is_future_event(start=event[0]):
             future_events.append(event)
 
-    return render_template("template.html", current_name=current_event[2].title(), future_name=future_events[0][2].title(),
-                           current_start = "{0}:{1}".format(current_event[0].hour, current_event[0].minute),
-                           current_end = "{0}:{1}".format(current_event[1].hour, current_event[1].minute),
-                           future_start = "{0}:{1}".format(future_events[0][0].hour, future_events[0][0].minute),
-                           future_end = "{0}:{1}".format(future_events[0][1].hour, future_events[0][1].minute))
+    if current_event:
+        current_name = current_event[2].title()
+        current_start = "{0}:{1:02d}".format(current_event[0].hour, current_event[0].minute)
+        current_end = "{0}:{1:02d}".format(current_event[1].hour, current_event[1].minute)
+        current_times = "({0} - {1})".format(current_start, current_end)
 
+    if future_events:
+        future_name = future_events[0][2].title()
+        future_start = "{0}:{1:02d}".format(future_events[0][0].hour, future_events[0][0].minute)
+        future_end = "{0}:{1:02d}".format(future_events[0][1].hour, future_events[0][1].minute)
+        upcoming_event = "({0} - {1}) {2}".format(future_start, future_end, future_name)
+
+    return render_template("template.html", 
+                           current_event_name=current_name, 
+                           current_event_times=current_times, 
+                           upcoming_event=upcoming_event)
+    
 app.run(host="0.0.0.0", port=6969)
