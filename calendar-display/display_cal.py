3d4dfc9bd41725f846f685ccc9d7843906f662f1
Sahil Goonraz <sahilgoonrazk@gmail.com>
2024-05-30 11:58:35 +0100

    moved event parsing functions to quickstart.py; adjusted get_events_info docstring to fit the project

--- calendar-display/display_cal.py
+++ calendar-display/display_cal.py
@@ -1,73 +1,11 @@
 from flask import Flask, render_template
-from quickstart import get_events_info
+from quickstart import get_events_info, is_event_allday, is_event_ongoing, is_cam_on, is_future_event
 import datetime
 import calendar
 
 app = Flask(__name__)
 month = datetime.datetime.now().month
 year = datetime.datetime.now().year
-
-def is_event_ongoing(start, end):
-    """
-    Checks if an event is ongoing by comparing the current time to the event start and end times.
-
-    Parameters:
-    -----------
-        start (datetime) : the start time of the specified event.
-        end (datetime) : the end time of the specified event.
-
-    Returns:
-    --------
-        (bool) : whether the given event is ongoing or not.
-    """
-    return start.replace(tzinfo=None) <= datetime.datetime.now() <= end.replace(tzinfo=None)
-
-def is_event_allday(start, end):
-    """
-    Checks if an event lasts all day by checking if both the start and end times are midnight.
-
-    Parameters:
-    -----------
-        start (datetime) : the start time of the specified event.
-        end (datetime) : the end time of the specified event.
-
-    Returns:
-    --------
-        (bool) : whether the given event lasts all day or not.
-    """
-    return start.hour == 0 and start.minute == 0 and end.hour == 0 and end.minute == 0
-
-def is_future_event(start):
-    """
-    Checks if an event is upcoming by comparing the start time against current time.
-
-    Parameters:
-    -----------
-        start (datetime) : the start time of the specified event.
-
-    Returns:
-    --------
-        (bool) : whether the given event is upcoming or not.
-    """
-    return datetime.datetime.now() < start.replace(tzinfo=None)
-
-def is_cam_on(event_name):
-    """
-    Checks if an event requires a webcam by looking for specific words in the event name.
-
-    Parameters:
-    -----------
-        event_name (str) : the name of the specified event.
-
-    Returns:
-    --------
-        (bool) : whether the given event is requires a webcam or not.
-    """
-    # cam_on_events is a list of strings that when found in the event name indicate the need for a cam
-    cam_on_events = ["meeting", "interview", "pmt", "webcam", "camera"]
-
-    # Returns whether any keyword was found in the event name
-    return any([keyword in event_name.lower() for keyword in cam_on_events])
 
 @app.route("/")
 def index():
--- calendar-display/quickstart.py
+++ calendar-display/quickstart.py
@@ -15,18 +15,11 @@ SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 def get_events_info():
   """
-  Returns a list containing different info partaining to the lessons
-  taught in a given month/year.
-
-  Arguments:
-    month (int)       : month for which to create dictionary for (e.g. for March enter 3).
-    year (int)        : year for which to create dictionary for (e.g. for 2024 enter 2024).
-    control_str (str) : the function will check for the specified string in the event name, and only treat
-                        it as a lesson if the control string is present. The control string will be removed
-                        from the event name, leaving only the student's name.
+  Returns a list containing all the events in the current day.
 
   Returns:
-    lessons_info (list : list containing sublists made of a lesson's date, student and duration.
+  --------
+    events_info (list) : list containing sublists made of an event's start and end time, and its name.
   """
   creds = None
   # The file token.json stores the user's access and refresh tokens, and is
@@ -87,3 +80,65 @@ def get_events_info():
 
   except HttpError as error:
     print(f"An error occurred: {error}")
+
+def is_event_ongoing(start, end):
+  """
+  Checks if an event is ongoing by comparing the current time to the event start and end times.
+
+  Parameters:
+  -----------
+    start (datetime) : the start time of the specified event.
+    end (datetime) : the end time of the specified event.
+
+  Returns:
+  --------
+    (bool) : whether the given event is ongoing or not.
+  """
+  return start.replace(tzinfo=None) <= datetime.datetime.now() <= end.replace(tzinfo=None)
+
+def is_event_allday(start, end):
+  """
+  Checks if an event lasts all day by checking if both the start and end times are midnight.
+
+  Parameters:
+  -----------
+    start (datetime) : the start time of the specified event.
+    end (datetime) : the end time of the specified event.
+
+  Returns:
+  --------
+    (bool) : whether the given event lasts all day or not.
+  """
+  return start.hour == 0 and start.minute == 0 and end.hour == 0 and end.minute == 0
+
+def is_future_event(start):
+  """
+  Checks if an event is upcoming by comparing the start time against current time.
+
+  Parameters:
+  -----------
+    start (datetime) : the start time of the specified event.
+
+  Returns:
+  --------
+    (bool) : whether the given event is upcoming or not.
+  """
+  return datetime.datetime.now() < start.replace(tzinfo=None)
+
+def is_cam_on(event_name):
+  """
+  Checks if an event requires a webcam by looking for specific words in the event name.
+
+  Parameters:
+  -----------
+    event_name (str) : the name of the specified event.
+
+  Returns:
+  --------
+    (bool) : whether the given event is requires a webcam or not.
+  """
+  # cam_on_events is a list of strings that when found in the event name indicate the need for a cam
+  cam_on_events = ["meeting", "interview", "pmt", "webcam", "camera"]
+
+  # Returns whether any keyword was found in the event name
+  return any([keyword in event_name.lower() for keyword in cam_on_events])
